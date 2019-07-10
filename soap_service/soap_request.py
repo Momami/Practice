64018dfe70b3b58f4d81244552c8002dce18b5b2
MoMaMi <milenochka.andreevna@mail.ru>
2019-07-10 14:18:52 +0400

    Request on Server

--- soap_service/soap_request.py
+++ soap_service/soap_request.py
@@ -2,26 +2,28 @@ from zeep import Client
 
 
 def send_request(client, data):
-    r = client.service.checkText(**data)
+    r = client.service.CheckText(data)
     return r
 
-
+# обработка при прямом подключении к YandexSpeller
 def treatment_response(response, text):
     if response is None:
         from yandex_translate import YandexTranslate
-        API = 'trnsl.1.1.20190709T063903Z.7e39efa042c9543b.16ef16a3444c163c8ad03b4d4a2ce728be47a798'
+        API = 'trnsl.1.1.20190709T063903Z.7e39efa042c9543b' \
+              + '.16ef16a3444c163c8ad03b4d4a2ce728be47a798'
         translate = YandexTranslate(API)
-        return translate.translate(text, 'ru-en')
+        return translate.translate(text, 'ru')
     else:
         return None
 
-
+# обработка при работе с композитом
 def treatment_response_composite(response, text):
-    if response is 'Success':
+    if response == 'Success':
         from yandex_translate import YandexTranslate
-        API = 'trnsl.1.1.20190709T063903Z.7e39efa042c9543b.16ef16a3444c163c8ad03b4d4a2ce728be47a798'
+        API = 'trnsl.1.1.20190709T063903Z.7e39efa042c9543b' \
+              + '.16ef16a3444c163c8ad03b4d4a2ce728be47a798'
         translate = YandexTranslate(API)
-        return translate.translate(text, 'ru-en')
+        return translate.translate(text, 'ru')
     else:
         return None
 
@@ -35,16 +37,17 @@ def treatment_translate(response):
 
 def main_function():
     text = input("Введите текст для перевода: ")
-    # cl = Client('http://speller.yandex.net/services/spellservice?WSDL')
-    cl = Client('http://DESKTOP-0M0S9AF:8088/mockSpellServiceSoap?WSDL')
-    req_data = {'text': text, 'lang': 'ru', 'options': 32,
-                'format': 'plain'}
-    r = send_request(cl, req_data)
-    translate = treatment_response(r, text)
+    wsdl = 'http://ulbs12-sales-app01.neoflex.ru:' \
+           + '8001/soa-infra/services/default/Project1/MySpellerWebService?WSDL'
+    cl = Client(wsdl)
+    # req_data = {'text': text, 'lang': 'en', 'options': 32,
+    #             'format': 'plain'}
+
+    r = send_request(cl, text)
+    # translate = treatment_response(r, text)
+    translate = treatment_response_composite(r, text)
     treatment_translate(translate)
 
 
 if __name__ == "__main__":
     main_function()
-
-
