43d5e71029f7d0247e12f00091f901990f33b190
MoMaMi <milenochka.andreevna@mail.ru>
2019-07-14 23:03:19 +0400

    Fix work with MSSQL Server and XPath-query

--- CheckValute/rate.py
+++ CheckValute/rate.py
@@ -1,10 +1,10 @@
 import xml.etree.ElementTree as ET
-import pyodbc
 from datetime import datetime
-from typing import Dict, Any
 from zeep import Client
 from zeep.wsdl.utils import etree_to_string
 import pandas as pd
+from sqlalchemy import create_engine
+from urllib import parse
 
 
 def recieve_valuteID(cnn):
@@ -14,64 +14,71 @@ def recieve_valuteID(cnn):
     valute_date = dict()
     for i in range(len(valutes)):
         valute_date[valutes[i]] = datetime.strptime(date[i], '%Y-%m-%d')
-    return valute_date
+    index = 0
+    return valute_date, index
 
 
 def connect_db():
-    server = 'DESKTOP-0M0S9AF'
-    database = 'ValuteDB'
-    driver = '{SQL Server}'  # Driver you need to connect to the database
-    cnn = pyodbc.connect(
-        'DRIVER=' + driver + ';PORT=port;SERVER=' + server + ';PORT=1443;DATABASE=' + database + '')
-    return cnn
+    params = parse.quote_plus(r'DRIVER={SQL Server};SERVER=DESKTOP-0M0S9AF;'
+                              r'DATABASE=ValuteDB;Trusted_Connection=yes')
+    conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+    engine = create_engine(conn_str)
+    return engine
 
 
-def find_rate_in_DB(valute_name, cnn):
+def find_rate_in_DB(valute_name, cnn, res):
     data = pd.read_sql("select RateVal from Rate where ValuteID = '" + valute_name + "'", cnn)
-    return valute_name, data['RateVal'][0]
+    #return valute_name, data['RateVal'][0]
+    res[valute_name] = data['RateVal'][0]
 
 
 def main_function():
     day_actual = 1
-    result, upgrade = [], []
+    result, upgrade = dict(), []
     title = "Введите валюты для получения курса: \n" \
             "(ввод осуществляется через пробел в одну строку)\n"
     valutes = input(title).split()
     date = datetime.strptime(input("Введите дату в формате DD-MM-YYYY: "),
                              '%d-%m-%Y')
     conn = connect_db()
-    val_day: Dict[Any, datetime] = recieve_valuteID(conn)
+    val_day, index = recieve_valuteID(conn)
     for valute in valutes:
         if valute in val_day.keys():
             if (date - val_day[valute]).days <= day_actual or \
                     (val_day[valute] - date).days > 0:
-                result.append(find_rate_in_DB(valute, conn))
+                find_rate_in_DB(valute, conn, result)
                 continue
         upgrade.append(valute)
-    mas_update = update_rate(upgrade, date)
+    mas_update, non_valid = update_rate(upgrade, date)
     for elem, rate in mas_update.items():
         if elem in val_day.keys():
             update_DB(elem, rate, date, conn)
         else:
             insert_DB(elem, rate, date, conn)
-        result.append((elem, rate))
+        result[elem] = rate
+    result.update(non_valid)
     print_result(result)
 
 
 def print_result(result):
     print("Курсы валют\n")
-    for elem, rate in result:
+    for elem, rate in result.items():
         print('{0}: {1}'.format(elem, rate))
 
 
 def update_DB(elem, rate, date, conn):
-    pd.read_sql("update Rate SET RateVal = '" + rate +
-                "', RateDate = '" + date + "' WHERE ValuteID = '" + elem + "'", conn)
+    data = pd.read_sql("select * from Rate", conn)
+    dt = str(date.date())
+    data.loc[data['ValuteID'] == elem, 'RateVal'] = [rate]
+    data.loc[data['ValuteID'] == elem, 'RateDate'] = [dt]
+    data.reset_index(drop=True, inplace=True)
+    data.to_sql("Rate", conn, if_exists="replace", index=False)
 
 
 def insert_DB(elem, rate, date, conn):
-    pd.read_sql("insert into Rate (ValuteID, RateVal, RateDate) VALUES ('" +
-                elem + "', '" + rate + "', '" + date + "')", conn)
+    dt = str(date.date())
+    df1 = pd.DataFrame.from_dict({'ValuteID': [elem], 'RateVal': [rate], 'RateDate': [dt]})
+    df1.to_sql("Rate", conn, if_exists='append', index=False)
 
 
 def send_request(cl, data):
@@ -86,54 +93,53 @@ def send_enumXML(cl):
 
 def enum_valutes(cl, upg):
     res = []
-    exc = []
+    exc = dict()
     response = send_enumXML(cl)
     dom = ET.fromstring(etree_to_string(response).decode())
     for elem in upg:
         current = dom.findall(
-            './[VCharCode = "' + elem + '"]'
+            './EnumValutes[VcharCode="' + elem + '"]'
         )
-        if not current is None:
+        if current:
             res.append(elem)
         else:
-            exc.append((elem, "Not Found"))
+            exc[elem] = "Not Found"
     return res, exc
 
 
 def update_rate(upgrade, date):
     client = Client("http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx?WSDL")
-    date = datetime.now()
-    # validate_valute, non_validate = enum_valutes(client, upgrade)
+    validate_valute, non_validate = enum_valutes(client, upgrade)
 
     response = send_request(client, date)
 
     dom = ET.fromstring(etree_to_string(response).decode())
-    currencies = []
+
     needed_currencies = dict()
-    print(upgrade)
-    for elem in upgrade:
+    for elem in validate_valute:
         new_elem = dom.findall(
-            './ValuteCursOnDate[VchCode = "' + elem + '"]'  # /Vcurs
+            "./ValuteCursOnDate[VchCode='" + elem + "']"  # /Vcurs
             # , namespaces
         )
-        if not new_elem is None:
-            currencies.append(new_elem)
-        else:
-            needed_currencies[elem] = 'Not Found'
+       # if new_elem is None:
+       #     needed_currencies[elem] = 'Not Found'
+        for valute in new_elem:
+            name = ''
+            value = ''
+            for node in valute.getiterator():
+                if node.tag == 'VchCode':
+                    name = node.text
+                if node.tag == 'Vcurs':
+                    value = node.text
+            needed_currencies[name] = value
 
-    for valute in currencies:
-        name = ''
-        value = ''
-        for node in valute.getiterator():
-            if node.tag == 'VchCode':
-                name = node.text
-            if node.tag == 'Vcurs':
-                value = node.text
-        needed_currencies[name] = value
+    #for name, value in needed_currencies.items():
+    #    print(name + ': ' + value)
 
-    for name, value in needed_currencies.items():
-        print(name + ': ' + value)
-    return needed_currencies
+    return needed_currencies, non_validate
+
+
+def update_rate_composite(upgrade, date): pass
 
 
 if __name__ == "__main__":
