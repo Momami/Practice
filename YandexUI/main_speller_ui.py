5269bd017dfd7bf40e07d6cf78683a4ad2fc839a
MoMaMi <milenochka.andreevna@mail.ru>
2019-07-20 19:07:16 +0400

    Add UI

---
+++ YandexUI/main_speller_ui.py
@@ -0,0 +1,48 @@
+# all the imports
+#from __future__ import with_statement
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+from contextlib import closing
+import soap_request
+
+# configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+
+# create our little application :)
+app = Flask(__name__, static_url_path='',
+            static_folder='CheckValute/static')
+app.config.from_object(__name__)
+langs = None
+
+
+@app.route('/')
+def show_entries():
+    langs = soap_request.get_lang()
+    return render_template('layout.html', langs=langs)
+
+
+@app.route('/translate', methods=['GET', 'POST'])
+def translate():
+    langs = soap_request.get_lang()
+    error = None
+    if request.method == 'POST':
+        if request.form['translate'] != '':
+            response = soap_request.main_ui(request.form['translate'],
+                                            request.form['language'])
+            if response is not None:
+                return render_template('layout.html', langs=langs, response=response)
+            else:
+                error = 'Введен текст с ошибками!'
+        else:
+            error = 'Введите текст для перевода!'
+    return render_template('layout.html', error=error, langs=langs)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ YandexUI/soap_request.py
@@ -0,0 +1,110 @@
+from zeep import Client
+from yandex_translate import YandexTranslate
+
+list_lang = None
+
+
+def send_request(client, data):
+    r = client.service.CheckText(data)
+    return r
+
+def send_request_not_comp(client, data):
+    r = client.service.checkText(**data)
+    return r
+
+def treatment_opt(text):
+    API = 'trnsl.1.1.20190709T063903Z.7e39efa042c9543b' \
+          + '.16ef16a3444c163c8ad03b4d4a2ce728be47a798'
+    return YandexTranslate(API)
+
+
+def treatment(text):
+    translate = treatment_opt(text)
+    print("Выберите целевой язык: \n")
+    list_lang = list(translate.langs)
+    for num, lan in enumerate(list_lang):
+        print('{} - {}'.format(num, lan), end=' ')
+        if (num + 1) % 5 == 0:
+            print()
+    print()
+    lang = input()
+    while not lang.isdigit() and int(lang) > 32:
+        lang = input("Неверно введен номер языка! \n Попробуйте еще раз.\n")
+    return translate.translate(text, list_lang[int(lang)])
+
+
+def treat(text, lang):
+    translate = treatment_opt(text)
+    return translate.translate(text, lang)
+
+
+# обработка при прямом подключении к YandexSpeller
+def treatment_response(response, text, lang):
+    if response is None:
+        return treat(text, lang)
+    else:
+        return None
+
+
+# обработка при работе с композитом
+def treatment_response_composite(response, text):
+    if response == 'Success':
+        return treatment(text)
+    else:
+        return None
+
+
+def treatment_translate(response):
+    if response is None:
+        print("Введён текст с ошибками!\n")
+    else:
+        print("Перевод: " + ' '.join(response['text']))
+
+
+def link_wsdl():
+    wsdl = 'http://speller.yandex.net/services/spellservice?WSDL'
+    cl = Client(wsdl)
+    return cl
+
+
+def main_ui(text, lang):
+    cl = link_wsdl()
+    req_data = {'text': text, 'lang': 'en', 'options': 32,
+                'format': 'plain'}
+    r = send_request_not_comp(cl, req_data)
+    translate = treatment_response(r, text, lang)
+    if translate is None:
+        return None
+    else:
+        return ' '.join(translate['text'])
+
+
+def main_function():
+    text = input("Введите текст для перевода: ")
+    wsdl = 'http://ulbs12-sales-app01.neoflex.ru:' \
+           + '8001/soa-infra/services/default/Project1/MySpellerWebService?WSDL'
+    wsdl_clean = 'http://speller.yandex.net/services/spellservice?WSDL'
+    # cl = Client(wsdl)
+    cl = Client(wsdl_clean)
+    req_data = {'text': text, 'lang': 'en', 'options': 32,
+                'format': 'plain'}
+
+    r = send_request_not_comp(cl, req_data)
+    # r = send_request(cl, text)
+    translate = treatment_response(r, text, 'ru')
+    # translate = treatment_response_composite(r, text)
+    treatment_translate(translate)
+
+
+def get_lang():
+    dict_lang = {'ru': 'Русский', 'de': 'Немецкий',
+                 'hy': '', 'es': 'Испанский',
+                 'pl': '', 'be': '',
+                 'sq': ''}
+    tr = treatment_opt('api')
+    list_lang = list(tr.langs)
+    return list_lang
+
+
+if __name__ == "__main__":
+    main_function()
